072b7d5aedaef23ae48b65f481675d3e195c20c9
Magd Bayoumi <git>
2019-01-16 18:57:36 -0500

    Minor Bug Fixes

--- TheFirstCrusade/util.py
+++ TheFirstCrusade/util.py
@@ -132,6 +132,25 @@ class Node():
             return False
         return self.x == other.x and self.y == other.y
 
+    def __str__(self):
+        return "(" + self.x + ", " + self.y + ")"
+
+def uniqify(seq, idfun=None): 
+   # order preserving
+   if idfun is None:
+       def idfun(x): return x
+   seen = {}
+   result = []
+   for item in seq:
+       marker = idfun(item)
+
+       if marker in seen:
+            continue
+
+       seen[marker] = 1
+       result.append(item)
+   return result
+
 def crossScale():
     return 1/3
 
